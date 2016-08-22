77ab98e0d63819292aea3e721590d87468276b30
Phil Benz <philbenz@gmail.com>
2016-08-22 10:03:59 -0600

    roughed in HTML of index

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    //prevent the event default
+    // event.preventDefault();
+
+    //sanity check
+    console.log('sanity check with galvanize eats index!');
+
+  }
+);
