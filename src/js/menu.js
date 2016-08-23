e8e440734e9f4fbe223a4321e4d84e142562c401
Phil Benz <philbenz@gmail.com>
2016-08-22 21:57:12 -0600

    yeah - that was brutal - functionally passing tho

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,6 +5,8 @@ $(document).on('ready', function() {
     //sanity check
     console.log('sanity check with galvanize eats menu!');
 
+    var menuItems = {};
+
     //ajax call to make sure you get the latest menu
     //this function is being used to populate the mapProp object for the map.
     $.ajax({
@@ -29,23 +31,51 @@ $(document).on('ready', function() {
     $( "#addItemButton" ).click(function() {
       //do something here
       quantity = $( '#quantity' ).val();
-      console.log('quantity: ', quantity);
+
+      if ($( '#subAmount' ).text() < 1 ) {
+        subtotal = 0;
+      } else {
+        subtotal = $( '#subAmount' ).text();
+      }
 
       if( quantity < 1 ) {
         quantity = 1;
       }
 
-      $('#orderSummary').append('<option>' + $( "#foodChoices option:selected" ).text() + '</option>');
+      for (var i=0; i < quantity; i++) {
+        $('#orderSummary').append("<option disabled>" + $( "#foodChoices option:selected" ).text() + '</option>');
 
-      //set order totals
-      subtotal = $( '#subAmount' ).val();
-      $( '#subAmount' ).text('');
 
+        //if you've ever drive by a hogfarm - this is what my code would smell like - but this is what happens when there's a deadline of 1 day
+        var str = $( "#foodChoices option:selected" ).text();
+        var search = str.search(/\d/)
+
+        //set order totals
+        var price = parseFloat(str.substr(search, str.length));
+
+        subtotal = parseFloat(subtotal) + price;
+
+        $( '#subAmount' ).text(subtotal.toFixed(2));
+
+        var tax = parseFloat(subtotal) * .07;
+        $( '#taxAmount' ).text(tax.toFixed(2));
+
+        var totalAmount = subtotal + tax;
+        $( '#totalAmount' ).text(totalAmount.toFixed(2));
+      }
       //set quantity field to 0!
       $( '#quantity' ).val('');
 
+    });
 
+    $( "#submitOrderButton" ).click(function() {
+      $.ajax({
+        method: 'POST',
+        url: 'https://galvanize-eats-api.herokuapp.com/orders'
+      }).done(function(results) {
 
+        console.log('POST: ', results);
+      });
     });
 
   });
